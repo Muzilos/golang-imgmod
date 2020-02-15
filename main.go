fcd1904564177e21ba76d2b7b950dd472d54318a
muzilos <smm321@gmail.com>
2020-02-15 17:13:56 -0500

    added bmp file support

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"golang.org/x/image/bmp"
 	"image"
 	"image/color"
 	"image/jpeg"
@@ -17,55 +18,62 @@ type convert func(uint8) uint8
 
 func main() {
 	var infile string
-	flag.StringVar(&infile, "i", "./img/painting.jpg", "jpeg format image file to read")
-	flag.StringVar(&infile, "input", "./img/painting.jpg", "jpeg format image file to read")
+	flag.StringVar(&infile, "i", "./img/painting.jpg", "image file to read")
+	flag.StringVar(&infile, "input", "./img/painting.jpg", "image file to read")
 	var outfile string
-	flag.StringVar(&outfile, "o", "./img/modded.jpg", "jpeg format filename for output")
-	flag.StringVar(&outfile, "output", "./img/modded.jpg", "jpeg format filename for output")
+	flag.StringVar(&outfile, "o", "./img/modded.jpg", "filename for output")
+	flag.StringVar(&outfile, "output", "./img/modded.jpg", "filename for output")
+	var imgtype string
+	flag.StringVar(&imgtype, "t", "jpeg", "file format (jpeg, bmp)")
+	flag.StringVar(&imgtype, "type", "jpeg", "file format (jpeg, bmp)")
+
 	flag.Parse()
 
 	fmt.Println("Reading: ", infile)
 	fmt.Println("Writing: ", outfile)
-	img := loadImage(infile)
+	img := loadImage(infile, imgtype)
 	imgPixels := getPixels(img)
 
 	var moddedImage = modImage(imgPixels, img.Bounds().Dx(), img.Bounds().Dy(), pixelMod)
-	writeImage(outfile, moddedImage)
+	writeImage(outfile, moddedImage, imgtype)
 	// for i, pixel := range getPixels(moddedImage) {
 	// 	fmt.Println("Pixel", i, "\t r g b a:", pixel)
 	// }
 }
 
 func pixelMod(p uint8) uint8 {
-	return uint8(float64(p) / 1.2)
+	return uint8(float64(p) * 3.0)
 }
 
-func loadImage(filename string) image.Image {
+func loadImage(filename string, imgtype string) image.Image {
 	f, err := os.Open((filename))
-	if err != nil {
-		log.Fatal(err)
-	}
+	var img image.Image
 	defer f.Close()
-
-	img, err := jpeg.Decode(f)
+	if imgtype == "bmp" {
+		img, err = bmp.Decode(f)
+	} else {
+		img, err = jpeg.Decode(f)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
 	return img
 }
 
-func writeImage(filename string, img image.Image) {
+func writeImage(filename string, img image.Image, imgtype string) {
 	outfile, err := os.Create((filename))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer outfile.Close()
-
-	var opt jpeg.Options
-	opt.Quality = 100
-	// ok, write out the data into the new JPEG file
-
-	err = jpeg.Encode(outfile, img, &opt) // put quality to 80%
+	if imgtype == "jpeg" {
+		var opt jpeg.Options
+		opt.Quality = 100
+		// ok, write out the data into the new JPEG file
+		err = jpeg.Encode(outfile, img, &opt) // put quality to 80%
+	} else if imgtype == "bmp" {
+		err = bmp.Encode(outfile, img)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
